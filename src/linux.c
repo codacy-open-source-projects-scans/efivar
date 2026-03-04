86ddebbaa31497858507d4c15b85be88a76d88ef
Codacy Archive <codacy-archive@local>
2026-03-04 01:12:11 +0000

    snapshot 2026-03-04T01:12:11.600331+00:00

--- src/linux.c
+++ src/linux.c
@@ -19,6 +19,9 @@
 #include <scsi/scsi.h>
 #include <stdbool.h>
 #include <stdio.h>
+#if defined(__FreeBSD__)
+#  include <sys/disk.h>
+#endif
 #include <sys/ioctl.h>
 #include <sys/mount.h>
 #include <sys/socket.h>
@@ -31,7 +34,7 @@
 #include "efiboot.h"
 
 int HIDDEN
-find_parent_devpath(const char * const child, char **parent)
+find_parent_devpath(char * const child, char **parent)
 {
 	int ret;
 	char *node;
@@ -707,7 +710,11 @@ get_sector_size(int filedes)
 {
 	int rc, sector_size = 512;
 
+#if defined(__linux__)
 	rc = ioctl(filedes, BLKSSZGET, &sector_size);
+#elif defined(__FreeBSD__)
+	rc = ioctl(filedes, DIOCGSECTORSIZE, &sector_size);
+#endif
 	if (rc)
 	        sector_size = 512;
 	return sector_size;
